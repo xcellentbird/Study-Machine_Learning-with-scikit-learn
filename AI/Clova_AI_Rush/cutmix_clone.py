f28b09810864e8ba18a7c4b443db0e8b69a663b7
Michael Kim <59414764+xcellentbird@users.noreply.github.com>
2021-06-22 21:46:56 +0900

    Add files via upload

---
+++ AI/Clova_AI_Rush/cutmix_clone.py
@@ -0,0 +1,35 @@
+""" source: https://github.com/clovaai/CutMix-PyTorch """
+
+def rand_bbox(size, lam):
+    W = size[2]
+    H = size[3]
+    cut_rat = np.sqrt(1. - lam)
+    cut_w = np.int(W * cut_rat)
+    cut_h = np.int(H * cut_rat)
+
+    # uniform
+    cx = np.random.randint(W)
+    cy = np.random.randint(H)
+
+    bbx1 = np.clip(cx - cut_w // 2, 0, W)
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = np.clip(cx + cut_w // 2, 0, W)
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+    return bbx1, bby1, bbx2, bby2
+
+
+def cutmix(input, target):
+    input = input.clone().detach()
+    target = target.clone().detach()
+    beta = 1.0
+    cutmix_prob = 1.0
+    lam = np.random.beta(beta, beta)
+    rand_index = torch.randperm(input.size()[0]).cuda()
+    target_a = target
+    target_b = target[rand_index]
+    bbx1, bby1, bbx2, bby2 = rand_bbox(input.size(), lam)
+    input[:, :, bbx1:bbx2, bby1:bby2] = input[rand_index, :, bbx1:bbx2, bby1:bby2]
+
+    lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (input.size()[-1] * input.size()[-2]))
+    return input, target_b, lam
---
+++ AI/KDT AI/README.md
@@ -0,0 +1,1 @@
+보조 사이트: https://datascienceschool.net/02%20mathematics/03.04%20%ED%8A%B9%EC%9E%87%EA%B0%92%20%EB%B6%84%ED%95%B4.html
