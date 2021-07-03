cc62d1ee01adb6d5e956df7e71b1d416e189acc3
Michael Kim <59414764+xcellentbird@users.noreply.github.com>
2021-07-04 02:58:29 +0900

    Add files via upload

---
+++ React Native/Do it 리액트 프로그래밍/4. 컴포넌트/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {SafeAreaView, Text} from 'react-native'
+import * as D from './src/data'
+
+const person = D.createRandomPerson()
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>
+        {JSON.stringify(person, null, 1)}
+      </Text>
+    </SafeAreaView>
+  )
+}
